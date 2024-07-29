5313af5fc827fab958430882509d09c88aeaeae0
123 <123@12.com>
2024-07-29 20:39:45 +0500

    start deploy 0.5

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     # Включить URL авторизации по умолчанию
-    path('', include('django.contrib.auth.urls')),
+    path('login/', views.login_view, name='login'),
     # Страница регистрации
     path('register/', views.register, name='register'),
     # Страница выхода
--- users/views.py
+++ users/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout, login
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('num_games:index')
+        else:
+            context = {'error':True}
+            return render(request, 'users/login.html', context)
+    else:
+        return render(request, 'users/login.html')
 
 def logout_view(request):
     logout(request)
